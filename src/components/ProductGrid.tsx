343221cbf97cb67b2ccd62bb7f0adb81a36bfe75
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2024-04-22 20:13:46 +0200

    feat: added pagination to ProductGrid

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,20 +1,56 @@
-import { Grid } from "@chakra-ui/react";
+import { Button, Grid, HStack } from "@chakra-ui/react";
 import ProductCard from "./ProductCard";
 import useProducts from "../hooks/useProducts";
+import { useState } from "react";
 
 const ProductGrid = () => {
-  const { data: products, error, isLoading } = useProducts();
+  const [page, setPage] = useState(1);
+  const pageSize = 12;
+  const {
+    data: products,
+    error,
+    isLoading,
+    isPlaceholderData,
+  } = useProducts({ page, pageSize });
+
+  const hasNextPage = products?.length == pageSize;
 
   if (isLoading) return <p>Loading products...</p>;
 
   if (error) return <p>{error.message}</p>;
 
   return (
-    <Grid templateColumns="repeat(4, 1fr)" gap={5}>
-      {products?.map((product) => (
-        <ProductCard product={product} />
-      ))}
-    </Grid>
+    <>
+      <Grid templateColumns="repeat(4, 1fr)" gap={5}>
+        {products?.map((product) => (
+          <ProductCard product={product} />
+        ))}
+      </Grid>
+      <HStack marginY={2}>
+        <Button
+          onClick={() => {
+            setPage(page - 1);
+            window.scrollTo(0, 0);
+          }}
+          isDisabled={page === 1}
+          className="btn btn-primary mt-3"
+        >
+          Previous
+        </Button>
+        <Button
+          onClick={() => {
+            if (!isPlaceholderData && hasNextPage) {
+              setPage(page + 1);
+              window.scrollTo(0, 0);
+            }
+          }}
+          isDisabled={isPlaceholderData || !hasNextPage}
+          className="btn btn-primary mt-3 ms-2"
+        >
+          Next
+        </Button>
+      </HStack>
+    </>
   );
 };
 
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,10 +1,15 @@
 import { Product } from "../interfaces/product";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
-const useProducts = () => {
+interface ProductQuery {
+    page: number,
+    pageSize: number
+}
+
+const useProducts = (productQuery: ProductQuery) => {
     const product: Product = {
-        Name: "Test",
-        Price: 199,
+        name: "Test",
+        price: 199,
       };
     
       function GetProducts(): Product[] {
@@ -12,11 +17,13 @@ const useProducts = () => {
         for (let index = 0; index < 20; index++) {
           products.push(product);
         }
-    
-        return products;
+        
+        const pageStartIndex = (productQuery.page - 1) * productQuery.pageSize;
+        const pageEndIndex = (productQuery.page - 1) * productQuery.pageSize + productQuery.pageSize;
+        return products.slice(pageStartIndex, pageEndIndex);
       }
     
-    return useQuery<Product[], Error>({queryKey: ["products"], queryFn: GetProducts})
+    return useQuery<Product[], Error>({queryKey: ["products", productQuery], queryFn: GetProducts, placeholderData: keepPreviousData})
 }
 
 export default useProducts
